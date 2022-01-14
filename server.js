bb74145d0bbef3296cbc46e32f69afd9da8d9a59
Emmanuel Onyeyaforo <rahzy24@gmail.com>
2022-01-14 23:09:50 +0100

    error, errorResponse

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config({ path: "./config.env" });
 const express = require('express');
 const connectDB = require('./config/db');
+const errorHandler = require('./middlewares/error')
 
 // Connect DB
 connectDB();
@@ -10,6 +11,9 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth'));
+
+// Error Handler ( Should be last piece of middleware)
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 4000
 
