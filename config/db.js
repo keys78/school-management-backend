ddad4e6355dd639d862168a87296c8e95944b41a
Emmanuel Onyeyaforo <rahzy24@gmail.com>
2022-05-14 15:32:57 +0100

    MDB update

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const connectDB  = () => {
-        mongoose.connect('mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false', {
+        // mongoose.connect('mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false', {
+        mongoose.connect('mongodb+srv://Emmanuel:todo12345@mernapp.2kina.mongodb.net/e-school', {
         useNewUrlParser: true,
         // useCreateIndex: true,
         // useUnfiedTopology: true,
