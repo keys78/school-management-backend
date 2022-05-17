0d463fb8d8e8055df7d545da0c83bb420d71ffaa
Emmanuel Onyeyaforo <rahzy24@gmail.com>
2022-05-17 22:06:35 +0100

    dot-env update

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = () => {
     // mongoose.connect('mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false', {
-    mongoose.connect('mongodb+srv://Emmanuel:todo12345@mernapp.2kina.mongodb.net/e-school', {
+    mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         // useCreateIndex: true,
         // useUnfiedTopology: true,
